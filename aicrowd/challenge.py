7147fe1d27e8b87ade854b095a2257b775990fbd
Nikhil Rayaprolu <nikhil684@gmail.com>
2020-01-29 21:48:48 +0530

    fixes #4 challenge issue

--- aicrowd/challenge.py
+++ aicrowd/challenge.py
@@ -8,13 +8,12 @@ def challenge_command():
     pass
 
 @click.command(help="Init the project structure for challenge")
-@click.option('--challenge', '-c')
+@click.argument('challenge')
 @pass_info
 def init(_: Info, challenge):
     """Say 'hello' to the nice people."""
     click.echo(f"generating the directory for challenge %s" %(challenge))
-    challenge_id = click.prompt('Please enter the challenge id', type=str)
-    challenge_id = Challenge(challenge_id).get_challenge_project()
+    Challenge(challenge).get_challenge_project()
     click.echo(f"generated the directory for the challenge %s" % (challenge))
 
 challenge_command.add_command(init)
