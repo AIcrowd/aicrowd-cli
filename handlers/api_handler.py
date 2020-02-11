d9470931a6f91191e3cce860b930b7c6c63196f8
Nikhil Rayaprolu <nikhil684@gmail.com>
2020-02-11 17:31:21 +0530

    final changes

--- handlers/api_handler.py
+++ handlers/api_handler.py
@@ -6,7 +6,7 @@ from requests import HTTPError
 
 headers = {'Content-Type': 'application/json'}
 
-AICROWD_API = 'http://51.79.142.104:8002/api'
+AICROWD_API = 'http://51.79.142.104/api'
 CHALLENGE_ROUTE = '/challenges/'
 BASELINE_ROUTE = '/baselines/'
 TEMPLATES_ROUTE = '/templates/'
