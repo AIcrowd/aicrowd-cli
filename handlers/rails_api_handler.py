4e3b8865b73e5675303fad3141bd1d99ea47dfdb
Akshat Chhajer <akshat.c2k@gmail.com>
2020-07-03 23:58:19 +0530

    Update rails_api_handler.py

--- handlers/rails_api_handler.py
+++ handlers/rails_api_handler.py
@@ -24,7 +24,7 @@ class RailsAPI:
         self.headers = {'Content-Type': 'application/json', 'Authorization': f'Token token="{auth_token}"'}
 
     def deploy_grader(self, challenge_slug, grader_id):
-        request_url = ''.join((self.api_endpoint, challenge_slug))
+        request_url = '/'.join((self.api_endpoint, challenge_slug))
         payload = {
             "evaluator_type_cd": "evaluations_api",
             "challenge_client_name": f"{grader_id}"
